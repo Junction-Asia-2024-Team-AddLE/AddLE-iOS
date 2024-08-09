32c03fa6b652338b5908ba28da736025ef6416de
Jeong Dong <eejxdong99@gmail.com>
2024-08-09 20:16:30 +0900

    chore: project init

---
+++ AddLE/AddLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddLEApp.swift
+//  AddLE
+//
+//  Created by 이정동 on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct AddLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
